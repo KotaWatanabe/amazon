be62a1ba1841376d97535a954d686667cd028a7b
Kota Watanabe <kota3316@gmail.com>
2019-04-05 14:57:22 -0700

    build home and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get("/", {to: "home#index", as: "root"})
+  get("/about", {to: "home#about"})
+  get("/contact_us", {to: "contact#new"})
+  post("/contact_us/create", {to: "contact#create"})
 end
